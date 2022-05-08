ca6299cfca30f5434bd9cd45e82c5550bf50037c
Sergio Alexander Florez Galeano <xergioalex@gmail.com>
2022-05-08 10:28:44 -0500

    Update config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://https://xergioalex.github.io/',
+  base: 'https://https://xergioalex.github.io/github-actions-course-platzi/',
   plugins: [react()]
 })
